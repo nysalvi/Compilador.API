ddf5b72aa58a92c53133fecbd1354ad28ab96182
nysalvi <nysalvi20@gmail.com>
2022-06-27 20:56:48 -0300

    .

--- ConsoleApp1/Token/TokenVisitor.cs
+++ ConsoleApp1/Token/TokenVisitor.cs
@@ -9,21 +9,42 @@ namespace Compilador.API.Token
     {
         public Dictionary<string, object?> Variables { get; } = new();
         public List<object?> Program { get; } = new();
+
+        public override object VisitPrograma([NotNull] TokenParser.ProgramaContext context)
+        {            
+            if (context is null)
+            {
+
+            }
+
+            if (context.GetChild(0) is not null && context.GetChild(0) != context.funcoes())
+                return "";
+            if (context.GetChild(0) is not null && context.GetChild(1) != context.principal())
+                return "";
+
+            return base.VisitPrograma(context);
+        }
+
         public override object? VisitAtribuicao([NotNull] TokenParser.AtribuicaoContext context)
         {
             var nomeVariavel = context.ID().GetText();
             var value = Visit(context.complemento());            
-            Variables[nomeVariavel] = value;
-            return value;
+            Variables[nomeVariavel] = nomeVariavel;
+            //return value;
+            return base.VisitAtribuicao(context);
         }
         public override object VisitTipo([NotNull] TokenParser.TipoContext context)
-        {            
-            if (context.GetChild(0) is not null && context.GetChild(0) == context.tipo_base())            
-                Visit(context.tipo_base());
-            else            
-                throw new Exception("");
+        {
+            if (context.GetChild(0) is not null && context.GetChild(0) != context.tipo_base())
+                return "";
+            //Visit(context.tipo_base());
+            else
+            {
+
+                //context.AddErrorNode();                
+            }
             
-            if (context.GetChild(1) is not null && context.GetChild(1) == context.dimensao())
+            if (context.GetChild(1) is not null && context.GetChild(1) != context.dimensao())
                 Visit(context.dimensao());
             else
                 throw new Exception("");
@@ -35,18 +56,40 @@ namespace Compilador.API.Token
             if (context is not null)
             {
                 if (context.GetChild(0) is not null && context.GetChild(0) == context.EPSILON())
+                {
                     return base.VisitDimensao(context);
+                }                    
                 if (context.GetChild(0) is not null && context.GetChild(0) == context.OPEN_BRACKET())
                     Program.Add(context.OPEN_BRACKET().GetText());
+                else
+                {
+
+                }
                 if (context.GetChild(1) is not null && context.GetChild(1) == context.INTEGER())
                     Program.Add(context.INTEGER().GetText());
+                else
+                {
+
+                }
                 if (context.GetChild(2) is not null && context.GetChild(2) == context.CLOSE_BRACKET())
                     Program.Add(context.CLOSE_BRACKET().GetText());
-                if (context.GetChild(3) is not null && context.GetChild(3) == context.dimensao())
-                    Visit(context.dimensao());
+                else
+                {
+
+                }
+                if (context.GetChild(3) is not null && context.GetChild(3) != context.dimensao())
+                {
+
+                }                    
             }
             // throw new Exception("");
             return base.VisitDimensao(context);
+        }
+        public override object VisitComplemento([NotNull] TokenParser.ComplementoContext context)
+        {
+            //expressao SEMICOLON | funcao SEMICOLON;
+
+            return base.VisitComplemento(context);
         }
         public override object VisitTipo_base([NotNull] TokenParser.Tipo_baseContext context)
         {
@@ -72,6 +115,7 @@ namespace Compilador.API.Token
             }
             return base.VisitTipo_base(context);
         }
+        
         private bool IsNull(ParserRuleContext parserRuleContext)
         {
             return parserRuleContext is null;
