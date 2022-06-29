0ce84e1580d094e8ebbecd1f87199705b193674b
Paulo Camargo <paulo13765@outlook.com>
2022-06-29 19:59:51 -0300

    Implementado algumas produções

--- ConsoleApp1/Token/TokenVisitor.cs
+++ ConsoleApp1/Token/TokenVisitor.cs
@@ -10,6 +10,8 @@ namespace Compilador.API.Token
         public Dictionary<string, object?> Variables { get; } = new();
         public List<object?> Program { get; } = new();
 
+        public List<string> Erros;
+
         public override object VisitPrograma([NotNull] TokenParser.ProgramaContext context)
         {            
             if (context is null)
@@ -115,10 +117,174 @@ namespace Compilador.API.Token
             }
             return base.VisitTipo_base(context);
         }
-        
-        private bool IsNull(ParserRuleContext parserRuleContext)
+
+        public override object VisitOp_aditivo([NotNull] TokenParser.Op_aditivoContext context)
         {
-            return parserRuleContext is null;
+
+            if (context is null)
+                Erros.Add("Nulo");
+
+            if (context.GetChild(0) == context.PLUS())
+                Program.Add(context.GetChild(0));
+            else if (context.GetChild(0) == context.MINUS())
+                Program.Add(context.GetChild(0));
+            else
+                Erros.Add("esperava: plus | minus");
+
+            return base.VisitOp_aditivo(context);
+        }
+
+        public override object VisitExpr_multiplicativa([NotNull] TokenParser.Expr_multiplicativaContext context)
+        {
+
+            if (context is null)
+                Erros.Add("Nulo");
+
+            if(context.GetChild(0) == context.fator() && context.GetChild(1) == context.expr_multiplicativa2())
+            {
+
+                Program.Add(context.GetChild(0));
+                Program.Add(context.GetChild(1));
+
+            }
+            else
+                Erros.Add("esperado: fator && expr_multiplicativa2");
+
+            
+            
+            return base.VisitExpr_multiplicativa(context);
+        }
+
+        public override object VisitExpr_multiplicativa2([NotNull] TokenParser.Expr_multiplicativa2Context context)
+        {
+            if (context is null)
+                Erros.Add("Nulo");
+
+            if (context.GetChild(0) == context.op_multiplicativo() && context.GetChild(1) == context.fator() && context.GetChild(2) == context.expr_multiplicativa2())
+            {
+
+                Program.Add(context.GetChild(0));
+                Program.Add(context.GetChild(1));
+                Program.Add(context.GetChild(2));
+
+            }
+            else if (context.GetChild(0) != context.EPSILON())
+                Erros.Add("esperado: episilon");
+            
+            return base.VisitExpr_multiplicativa2(context);
+        }
+
+        public override object VisitOp_multiplicativo([NotNull] TokenParser.Op_multiplicativoContext context)
+        {
+            if (context is null)
+                Erros.Add("Nulo");
+
+            if (context.GetChild(0) == context.TIMES())
+                Program.Add(context.GetChild(0));
+            else if (context.GetChild(0) == context.DIV())
+                Program.Add(context.GetChild(0));
+            else if (context.GetChild(0) == context.MOD())
+                Program.Add(context.GetChild(0));
+
+            else
+                Erros.Add("...");
+
+            return base.VisitOp_multiplicativo(context);
+        }
+
+        public override object VisitFator([NotNull] TokenParser.FatorContext context)
+        {
+            if (context is null)
+                Erros.Add("Nulo");
+
+            if(context.GetChild(0) == context.sinal() && context.GetChild(1) == context.ID() && context.GetChild(2) == context.vetor())
+            {
+
+                Program.Add(context.GetChild(0));
+                Program.Add(context.GetChild(1));
+                Program.Add(context.GetChild(2));
+
+            }else if(context.GetChild(0) == context.constante())
+            {
+
+                Program.Add(context.GetChild(0));
+
+            }else if(context.GetChild(0) == context.NOT() && context.GetChild(1) == context.fator())
+            {
+
+                Program.Add(context.GetChild(0));
+                Program.Add(context.GetChild(1));
+
+            }else if (context.GetChild(0) == context.OPEN_PARENTHESIS() && context.GetChild(1) == context.expressao() && context.GetChild(2) == context.CLOSE_PARENTHESIS())
+            {
+
+                Program.Add(context.GetChild(0));
+                Program.Add(context.GetChild(1));
+                Program.Add(context.GetChild(2));
+
+            }
+            else
+            {
+
+                Erros.Add("...");
+
+            }
+            
+            return base.VisitFator(context);
+        }
+
+        public override object VisitVetor([NotNull] TokenParser.VetorContext context)
+        {
+
+            if (context is null)
+                Erros.Add("Nulo");
+
+            if(context.GetChild(0) == context.OPEN_BRACKET() && context.GetChild(1) == context.expr_aditiva() && context.GetChild(2) == context.CLOSE_BRACKET())
+            {
+
+                Program.Add(context.GetChild(0));
+                Program.Add(context.GetChild(1));
+                Program.Add(context.GetChild(2));
+
+            }else if(context.GetChild(0) != context.EPSILON())
+                Erros.Add("esperado : [ sinal | constante | variavel | expressão entre () ]");
+
+
+            return base.VisitVetor(context);
+        }
+
+        public override object VisitConstante([NotNull] TokenParser.ConstanteContext context)
+        {
+            if (context is null)
+                Erros.Add("Nulo");
+
+            if(context.GetChild(0) == context.sinal() && context.GetChild(1) == context.INTEGER() || context.GetChild(0) == context.sinal() && context.GetChild(1) == context.DECIMAL())
+            {
+
+                    Program.Add(context.GetChild(0));
+                    Program.Add(context.GetChild(1));
+
+            }
+            else if(context.GetChild(0) == context.TEXT())
+                    Program.Add(context.TEXT().GetText());
+
+            return base.VisitConstante(context);
+        }
+
+        public override object VisitSinal([NotNull] TokenParser.SinalContext context)
+        {
+            if (context is null)
+                Erros.Add("nulo");
+
+            if (context.GetChild(0) == context.PLUS() || context.GetChild(0) == context.MINUS()) { 
+            
+                Program.Add(context.GetChild(0).GetText());
+            
+            }else if(context.GetChild(0) != context.EPSILON())
+                Erros.Add("esperado : + | -");
+
+            
+            return base.VisitSinal(context);
         }
     }
 }
